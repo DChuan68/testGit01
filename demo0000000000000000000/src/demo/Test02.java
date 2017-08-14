6361cf600a4f607516efb7519d12bcf5b10100bc
DChuan68 <dcwu520@outlook.com>
2017-08-14 19:37:17 +0800

    未

--- demo0000000000000000000/src/demo/Test02.java
+++ demo0000000000000000000/src/demo/Test02.java
@@ -4,12 +4,7 @@ public class Test02 {
 	public static void main(String[] args) {
 		
 		String s=new String("qwertyui");
-		System.out.println(s.substring(1));// wertyui 
-		System.out.println(s.substring(1));// wertyui 
-		System.out.println(s.charAt(6));   // q
-		System.out.println(s.charAt(7));   // q
-		System.out.println(s.charAt(7));   // q
-		System.out.println(s.charAt(6));   // q
+		String s1=new String("qwertyui");
 		
 	}
 	
