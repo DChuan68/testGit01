cdbcf160cdb023570cc52ef7e785a8fafcca6618
DChuan68 <dcwu520@outlook.com>
2017-08-14 18:18:05 +0800

    3

--- demo0000000000000000000/src/demo/Test02.java
+++ demo0000000000000000000/src/demo/Test02.java
@@ -5,7 +5,7 @@ public class Test02 {
 		
 		String s=new String("qwertyui");
 		System.out.println(s.substring(1));// wertyui 
-		System.out.println(s.charAt(0));   // q
+		System.out.println(s.charAt(3));   // q
 		
 	}
 	
