74dddaac93ae2775ba840d248ff2f102eb5dfe5f
DChuan68 <dcwu520@outlook.com>
2017-08-14 19:23:06 +0800

    5

--- demo0000000000000000000/src/demo/Test02.java
+++ demo0000000000000000000/src/demo/Test02.java
@@ -9,6 +9,7 @@ public class Test02 {
 		System.out.println(s.substring(1));// wertyui 
 		System.out.println(s.charAt(6));   // q
 		System.out.println(s.charAt(6));   // q
+		System.out.println(s.charAt(6));   // q
 		
 	}
 	
